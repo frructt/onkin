9143510729fff60418cfba9af0fbccf48745cfd6
konstantin <konstantin.gornostaev@dsr-corporation.com>
2020-04-19 22:45:05 +0300

    added user authentication

--- film_aggregator/__init__.py
+++ film_aggregator/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = '7104ba209f0cf2e63b28982f7b8782e8'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
 from film_aggregator import routes
