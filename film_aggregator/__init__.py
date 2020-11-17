9add4483f310785e0fba6f4b3cb9d9c0901d3257
konstantin <konstantin.gornostaev@dsr-corporation.com>
2020-11-17 18:58:29 +0300

    Changed db server path to MSSQL

--- film_aggregator/__init__.py
+++ film_aggregator/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = '7104ba209f0cf2e63b28982f7b8782e8'
-app.config["SQLALCHEMY_DATABASE_URI"] = "mssql+pyodbc://localhost\SQLEXPRESS/FilmsAggregator?driver=SQL Server?Trusted_Connection=yes"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mssql+pyodbc://admin:12345@83.167.115.245/test?driver=SQL Server?Trusted_Connection=yes"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
