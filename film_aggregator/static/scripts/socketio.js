aa338a80925ee96f8371c8608b80865aa329ba23
frructt <frructt@gmail.com>
2020-12-02 17:48:24 +0300

    Socket video failed test

--- film_aggregator/static/scripts/socketio.js
+++ film_aggregator/static/scripts/socketio.js
@@ -11,4 +11,38 @@ document.addEventListener('DOMContentLoaded', () => {
     socket.on('some event', data => {
         console.log(data);
     });
+
+    // var vid = document.getElementById("videoId");
+    // function myFunction() {
+    //     alert(vid.paused);
+    // }
+
+
+
+    document.getElementById("videoId").onplay = () => {
+        socket.send(document.getElementById("videoId").onpause)
+    }
+
+    socket.on('play_video', (data) => {
+        if (!data) {
+            document.getElementById("videoId").play();
+        } else {
+            document.getElementById("videoId").pause();
+        }
+        console.log(data);
+            // document.getElementById("videoId").play();
+    });
+
+    // socket.on('play video event', () => {
+    //     document.getElementById("videoId").play();
+    // });
+
+    // socket.on('play_video', data => {
+    //     var vid = document.getElementById("videoId");
+    //     socket.send("Got it");
+    //     vid.onplay = function() {
+    //         socket.send("The video has started to play");
+    //         console.log(`The video has started to play: ${data}`);
+    //     };
+    // })
 })
