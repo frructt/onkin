19a4a17c8c02b67fe70d6b5a1770458caeb82ffa
konstantin <konstantin.gornostaev@dsr-corporation.com>
2020-12-23 17:42:27 +0300

    test commit

--- server/models.py
+++ server/models.py
@@ -12,7 +12,7 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(25), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
     def to_json(self):
