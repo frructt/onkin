7d00b4d1f8f690377c965801a70a585eedc8a430
konstantin <konstantin.gornostaev@dsr-corporation.com>
2021-01-06 19:36:01 +0300

    Changed player.component by adding videogular custom controls

--- front/src/app/_services/video.service.ts
+++ front/src/app/_services/video.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from '@environments/environment';
-import { Video } from '@app/_models';
 
 @Injectable({ providedIn: 'root' })
 export class VideoService {
--- front/src/app/player/player.component.ts
+++ front/src/app/player/player.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { VideoService } from '@app/_services';
-import {first} from 'rxjs/operators';
-import { Observable, EMPTY, throwError, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import base64url from 'base64url';
-import {flatMap} from 'rxjs/internal/operators';
 import {HttpResponse} from '@angular/common/http';
-import {DomSanitizer} from '@angular/platform-browser';
 
 
 @Component({
@@ -18,10 +12,11 @@ import {DomSanitizer} from '@angular/platform-browser';
 export class PlayerComponent implements OnInit {
 
   videoName: string;
-  streamVideo;
-  streamVideoURL;
+  videoItem;
+  data;
 
-  constructor(private videoService: VideoService, private sanitizer: DomSanitizer) {
+
+  constructor(private videoService: VideoService) {
   }
 
   ngOnInit() {
@@ -30,25 +25,23 @@ export class PlayerComponent implements OnInit {
     });
     this.videoService.streamVideo('long_video1.mp4').subscribe(
       (response: HttpResponse<Blob>) => {
-        this.streamVideoURL = response.url;
-        // const binaryData = [];
-        // binaryData.push(response.body);
-        // this.streamVideoURL = URL.createObjectURL(new Blob(binaryData, { type: 'blob' }));
-        // this.sanitizer.bypassSecurityTrustResourceUrl(this.streamVideoURL);
-
-        // const cuttedPart = this.streamVideoURL.split('/');
-        // const decodedCuttedPart = atob(cuttedPart[3]);
-        // this.streamVideoURL = cuttedPart[0] + cuttedPart[1] + cuttedPart[2] + decodedCuttedPart;
-
-        // .streamVideoURL = base64url.decode(this.streamVideoURL)
+        this.videoItem = {
+          name: 'Video one',
+          src: response.url,
+          type: 'video/mp4'
+        };
       },
       error => console.log('oops!!!', error)
     );
   }
 
-  OnDestroy() {
-    URL.revokeObjectURL(this.streamVideoURL)
+  videoPlayerInit(data) {
+    this.data = data;
+
+    this.data.getDefaultMedia().subscriptions.loadedMetadata.subscribe(this.initVdo.bind(this));
   }
 
-
+  initVdo() {
+    this.data.play();
+  }
 }
